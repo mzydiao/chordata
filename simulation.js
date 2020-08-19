875432e45e9367bb130fba17ee014a6fa8891c57
Jason Yang <jason.yang31415@gmail.com>
2020-08-19 00:36:39 -0400

    deterministic hashes, json neighbors at every step

--- simulation.js
+++ simulation.js
@@ -1,10 +1,15 @@
 const m = 4;
 const ChordNode = require("./chord")(m);
+const fs = require("fs");
+
+const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
 let neighbors;
 let nodes;
 
-function simulate(numPeople) {
+async function simulate(numPeople) {
+	let object = [];
+
 	//start with a single node
 	neighbors = new Map();
 	nodes = new Map();
@@ -25,16 +30,49 @@ function simulate(numPeople) {
 				newGuy.join(connectTo);
 			});
 		}
+		await sleep(200);
+		object.push(obj(neighbors));
+		console.log(i);
 	}
+
+	fs.writeFile("data.json", JSON.stringify(object), (err) => {
+		console.error(err);
+	});
 }
 
+const hashes = [
+	"0x3597",
+	"0x45e4",
+	"0xe8aa",
+	"0xbaf5",
+	"0x7412",
+	"0xd28f",
+	"0x10ea",
+	"0x9b25",
+	"0x7e74",
+	"0xffa4",
+	"0x4af0",
+	"0x5993",
+	"0xd63e",
+	"0x01ee",
+	"0xba6f",
+	"0xa035",
+	"0x1a68",
+	"0x843b",
+	"0xeaa3",
+	"0xcad4",
+];
+let hashIndex = 0;
+
 function genHash() {
-	let currString = "0x";
+	/* let currString = "0x";
 	let convertHex = "0123456789abcdef";
 	for (let i = 0; i < m; i++) {
 		let rand = Math.floor(Math.random() * 16);
 		currString = currString + convertHex.substring(rand, rand + 1);
-	}
+    } */
+	let currString = hashes[hashIndex];
+	hashIndex++;
 	return currString;
 }
 
@@ -93,20 +131,20 @@ function generateFuncs(hash) {
 	};
 }
 
-function jsonify(map) {
+function obj(map) {
 	let obj = {};
 	map.forEach((v, k, m) => {
 		obj[k] = Array.from(v).map((bigint) => bigint.toString());
 	});
-	return JSON.stringify(obj);
+	return obj;
 }
 
 simulate(20);
-const fs = require("fs");
-setTimeout(() => {
+/* setTimeout(() => {
 	const json = jsonify(neighbors);
 	console.log(json);
 	fs.writeFile("data.json", json, (err) => {
 		console.error(err);
 	});
 }, 1000);
+ */
