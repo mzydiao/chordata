8b4e0080f60d17c143197b47fb42d8d8fd76dd00
Michael Diao <diaoeous@gmail.com>
2020-08-19 19:32:20 -0700

    SIM: Add comments to python simulation files.

--- check_adj.py
+++ check_adj.py
@@ -1,51 +1,69 @@
+# goal: check if the adjacency lists are right
+
 import json
 import os
 import numpy as np
 
-with open(os.path.join(os.getcwd(), 'data.json'), 'r') as f:
-    adj = json.load(f)
+# number of keys in simulation is 4 ** 8
+m = 8
 
-# check if the adjacency lists are right
-
-
+# check that each person has good finger table.
 def check_adj(adj):
-    m = 8
-    # check that each person has good finger table.
+    # get a list of the edges in the adjacency list
     edges = [(int(k), int(v)) for k, l in adj.items() for v in l]
+
+    # extract all distinct nodes in the graph in sorted order.
     all_nodes = {int(k) for k in adj}
     all_nodes = sorted(all_nodes)
-    should = {}
+
+    # keep track of the finger tables for each node.
+    fingers = {}
+
+    # iterate through nodes and calculate the finger tables.
     for node in all_nodes:
-        should[node] = set()
-        # make sure that finger table is right.
-        connected = {int(k) for k in adj[str(node)]}
-        # want to check that all fingers are in adj[node]
+        # initialize finger table for this node
+        fingers[node] = set()
+
+        # rotate the list of sorted nodes such that successor
+        # of the current node is first.
         rotated_nodes = [(n - node) % 4**m for n in all_nodes]
         my_index = rotated_nodes.index(0)
         rotated_nodes = rotated_nodes[my_index+1:] + rotated_nodes[:my_index]
+        
+        # calculate successor(node + 4 ** i), add to finger table.
         for i in range(m):
             plus = 4 ** i
-            # get node that is >= plus+node
-            # do rotation
-            for ele in rotated_nodes:
-                if ele >= plus:
-                    should[node].add((ele+node) % 4 ** m)
+            for increment in rotated_nodes:
+                # if increment >= plus, then (increment+node) % 4**m is
+                # in the finger table if it is encountered first.
+                if increment >= plus:
+                    fingers[node].add((increment + node) % 4 ** m)
                     break
-        if len(should[node] - connected) > 0:
-            return
-    should_edges = set()
-    for k, set_conns in should.items():
+
+    # construct a set of the edges that should exist due to fingers
+    fingers_edges = set()
+    for k, set_conns in fingers.items():
         for v in set_conns:
-            should_edges.add((k, v))
-            should_edges.add((v, k))
+            fingers_edges.add((k, v))
+            fingers_edges.add((v, k))
+
+    # look at the set of edges that actually exist
     set_edges = set(edges)
-    print(len(set_edges))
-    print(len(should_edges))
-    print(set_edges - should_edges)
-    if set_edges != should_edges:
-        print('bad')
-        return should_edges
-    print('good')
 
+    # print out results
+    print("length of edges list:", len(set_edges))
+    print("length of fingers list:", len(fingers_edges))
+    print("set difference edges-fingers:", set_edges - fingers_edges)
+    if set_edges != fingers_edges:
+        print("edges and fingers do not match")
+        print("fingers:", fingers_edges)
+        return 
+    print('good :D')
 
-print(check_adj(adj[-1]))
+if __name__ == '__main__':
+    #open the data.json file containing each adjacency dicts at each timestep
+    with open(os.path.join(os.getcwd(), 'data.json'), 'r') as f:
+        adj = json.load(f)
+    # run the adjacency check on the final graph
+
+    check_adj(adj[-1])
--- visualize_chord.py
+++ visualize_chord.py
@@ -1,50 +1,65 @@
+# goal: produce plots of the chord diagrams at each time step.
+
 import json
 import os
 import numpy as np
 import matplotlib.pyplot as plt
+import sys
 
-figs_path = os.path.join(os.getcwd(), 'figs')
-
-with open(os.path.join(os.getcwd(), 'data.json'), 'r') as f:
-    adj = json.load(f)
-
-
+# generate plots on ax given an adjacency dictionary
 def get_plots(adj, ax):
-    # generate plots given an adjacency matrix and an axis for plt
+    # retrieve the edges in the adjacency dictionary
     edges = [(int(k), int(v)) for k, l in adj.items() for v in l]
+
+    # extract a sorted list of all nodes
     all_nodes = {int(k) for k in adj}
     all_nodes = sorted(all_nodes)
 
     # print all the edges
     for a, b in edges:
-        k1, k2 = all_nodes.index(a), all_nodes.index(b)
-        degs = [k1/(len(all_nodes)) * 2 * np.pi,
-                k2/(len(all_nodes)) * 2 * np.pi]
+        # want to uniformly distribute the nodes on the circle
+        # so, we can position each node at (cos theta, sin theta)
+        # where theta is 2pi * node's index / total # nodes.
+        theta1, theta2 = all_nodes.index(a), all_nodes.index(b)
+        degs = [theta1/(len(all_nodes)) * 2 * np.pi,
+                theta2/(len(all_nodes)) * 2 * np.pi]
+
+        # connect a, b
         ax.plot(np.cos(degs), np.sin(degs))
 
     # label all the nodes
     for node in all_nodes:
+        # calculate the degree of location of the node
         deg = all_nodes.index(node)/(len(all_nodes)) * 2 * np.pi
+
+        # calculate coordinate given degree
         c, s = np.cos(deg)*1.1, np.sin(deg)*1.1
+
+        #label the coordinate
         ax.text(c, s, node,
                 horizontalalignment='center',
                 verticalalignment='center')
 
 
-# create figure object
-fig, axes = plt.subplots(
-    int(np.ceil(len(adj)/5)), 5,
-    figsize=(50, 2*(len(adj) + 5 - (len(adj) % 5)))
-)
+if __name__ == '__main__':
+    # read the adjacency matrix JSON file
+    figs_path = os.path.join(os.getcwd(), 'figs')
+    with open(os.path.join(os.getcwd(), 'data.json'), 'r') as f:
+        adj = json.load(f)
 
-# make plot
-for a, ax in zip(adj, axes.flatten()):
-    get_plots(a, ax)
-    ax.axis('off')
-# remove ax not in zip
-for ax in axes.flatten()[len(adj):]:
-    ax.axis('off')
+    # create figure object
+    fig, axes = plt.subplots(
+        int(np.ceil(len(adj)/5)), 5,
+        figsize=(50, 2*(len(adj) + 5 - (len(adj) % 5)))
+    )
 
-# save figures
-plt.savefig(os.path.join(figs_path, 'chord-fingers.png'))
-plt.show()
+    # make plot
+    for a, ax in zip(adj, axes.flatten()):
+        get_plots(a, ax)
+        ax.axis('off')
+    # remove ax not in zip
+    for ax in axes.flatten()[len(adj):]:
+        ax.axis('off')
+
+    # save figures
+    plt.savefig(os.path.join(figs_path, '%s.png' % sys.argv[-1]))
